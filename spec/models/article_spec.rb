ed14e49454190ad73f4042c8bd16336562fd4b57
Katharine Francis <katie-francis@hotmail.com>
2019-05-15 10:21:57 -0400

    Small text changes and updated ruby version

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe Article, type: :model do
   describe '.recent' do
     it 'should list recent article first' do
       old_article = create :article
-      newer_article = create : article
+      newer_article = create :article
       expect(described_class.recent).to eq(
         [ newer_article, old_article]
       )
