53fadab3c819769dcec9ab0b3399a3875c80edb8
Katharine Francis <katie-francis@hotmail.com>
2019-05-16 11:46:19 -0400

    Added notes

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -1,5 +1,8 @@
 class AccessTokensController < ApplicationController
+#Using the user_authenticator service in '../lib/user_authenticator.rb'
+#The 'perform' method is from this service
 
+#Also, a lot of helper methods were deposited in applications_controller.rb and we inherited them.
   def create
     authenticator = UserAuthenticator.new(params[:code])
     authenticator.perform
